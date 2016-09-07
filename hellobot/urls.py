8feb29b8d13c5b6d59d2f91497e8bb614bcc6da4
sang14 <sangya.14@gmail.com>
2016-09-07 17:09:58 +0530

    foo

---
+++ hellobot/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from chatbot.views import MyChatBotView, index
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'hellobot.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',index),
+    url(r'^facebook_auth/',MyChatBotView.as_view()),
+)
