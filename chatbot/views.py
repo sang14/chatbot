5475beac44089443e8820e54ec8879474494fd7e
sang14 <sangya.14@gmail.com>
2016-09-17 12:46:20 +0530

    foo

--- chatbot/views.py
+++ chatbot/views.py
@@ -82,6 +82,13 @@ def post_facebook_message(fbid,message_text):
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
 	print status.json()
 
+def handle_postback(fbid,payload):
+	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+	output_text='Payload Recieved: '+payload
+	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":output_text}})
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+
+
 class MyChatBotView(generic.View):
 	def get(self,request,*args,**kwargs):
 		if self.request.GET['hub.verify_token']==VERIFY_TOKEN:
@@ -99,7 +106,14 @@ class MyChatBotView(generic.View):
 
 		for entry in incoming_message['entry']:
 			for message in entry['messaging']:
-				print message
+				try:
+					if 'postback' in message:
+						handle_postback(message['sender']['id'],message['postback']['payload'])
+					else:
+						pass
+				except Exception as e:
+					print e
+				#print message
 				try:
 					sender_id = message['sender']['id']
 					message_text = message['message']['text']
